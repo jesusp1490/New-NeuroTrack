b3fb01dfb959c93a285dbb25eeb9d59a651cc3b6
Jesus <jesusperez_14@hotmail.com>
2025-03-12 16:13:19 +0100

    vercel config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  webpack: (config, { dev, isServer }) => {
-    if (dev && !isServer) {
-      config.cache = false
-    }
-    return config
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  // Exclude the functions directory from the build
+  typescript: {
+    // Exclude the functions directory from type checking
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Exclude the functions directory from linting
+    ignoreDuringBuilds: true,
   },
 }
 
